bb2969c91f639a36aaea77fce8a850964dcd2ffa
Kory <74568018+lopezkor000@users.noreply.github.com>
2025-04-23 22:08:40 -0500

    MORE DATA

--- dataCollection.py
+++ dataCollection.py
@@ -6,5 +6,5 @@ SP_500 = pd.read_csv('S&P_500.csv')
 for i, stock in SP_500.iterrows():
   symbol = stock['Symbol']
   ticker = yf.Ticker(symbol)
-  history = ticker.history(period='5y', interval='1d')
+  history = ticker.history(period='10y', interval='1d')
   history.to_csv(f'data/{symbol}.csv')
